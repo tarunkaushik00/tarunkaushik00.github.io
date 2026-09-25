3d51e8a9e111bb7b9c88ca435f4cb200dcde9f26
tarunkaushik09 <asuskaushik09@gamil.com>
2026-09-25 22:05:47 +0530

    Add resume.pdf

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,8 +34,8 @@ function Hero() {
             </a>
 
             <a
-              href="/tarun-portfolio/public/resume.pdf"
-              download
+              href="../assets/resume.pdf"
+              download="Tarun_Resume.pdf"
               className="inline-flex items-center gap-2 rounded-full border border-white/15 px-6 py-3 font-semibold text-white transition duration-300 hover:-translate-y-1 hover:border-cyan-400 hover:text-cyan-400"
             >
               Download Resume
