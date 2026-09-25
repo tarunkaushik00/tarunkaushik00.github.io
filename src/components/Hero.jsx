1970fd3310d07889316144bbea4d8bba05cbd9ca
tarunkaushik09 <asuskaushik09@gamil.com>
2026-09-25 22:46:09 +0530

    Initial Commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import { FaGithub, FaLinkedinIn, FaArrowDown } from "react-icons/fa";
 import tarunPhoto from "../assets/tarun.jpg";
-import resumeFile from "../assets/Projects/resume.pdf"
 
 function Hero() {
   return (
@@ -35,7 +34,7 @@ function Hero() {
             </a>
 
             <a
-              href={resumeFile}
+              href="./resume.pdf"
               download="Tarun-Resume.pdf"
               className="inline-flex items-center gap-2 rounded-full border border-white/15 px-6 py-3 font-semibold text-white transition duration-300 hover:-translate-y-1 hover:border-cyan-400 hover:text-cyan-400"
             >
